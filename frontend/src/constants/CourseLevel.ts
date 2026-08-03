a0bc8723265a51030e81df2082563ef242b980e0
wach78 <dev@wach.nu>
2026-08-03 15:03:53 +0200

    fix bug in CourseLevel

--- frontend/src/constants/CourseLevel.ts
+++ frontend/src/constants/CourseLevel.ts
@@ -1,7 +1,7 @@
 export const CourseLevel = {
-    Beginner: 0,
-    Intermediate: 1,
-    Advanced: 2
+    Beginner: 1,
+    Intermediate: 2,
+    Advanced: 3
 } as const;
 
 export type CourseLevelValue =

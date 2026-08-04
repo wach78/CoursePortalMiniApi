97187bf1e19ca8ffd94d2979a37813f8072c8d0c
Martin Leo <martin.christoffer.leo@gmail.com>
2026-08-04 09:57:19 +0200

    course details-view

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,7 @@
-
-import {
-    fetchCourses,
-    fetchCourse
-} from "./service/courseService";
+import { fetchCourses, fetchCourse } from "./service/courseService";
 
 import type { CourseResponseDto } from "./interfaces/CourseResponseDto";
+import { CourseLevelLabels } from "./constants/CourseLevel";
 
 type CourseSortBy = "price" | "level";
 type SortDirection = "asc" | "desc";
@@ -19,25 +16,28 @@ const getRequiredElement = <T extends HTMLElement>(id: string): T => {
     return element as T;
 };
 
-
-
 const coursesList = getRequiredElement<HTMLUListElement>("coursesList");
 const courseDetails = getRequiredElement<HTMLDivElement>("course-details");
 const courseSortSelect = getRequiredElement<HTMLSelectElement>("course-sort");
 
-
 const loadCourses = async (
     sortBy?: CourseSortBy,
-    direction: SortDirection = "asc"
+    direction: SortDirection = "asc",
 ): Promise<void> => {
     const courses = await fetchCourses(sortBy, direction);
 
     coursesList.replaceChildren();
 
-    courses.forEach(course => {
-
+    courses.forEach((course) => {
         const courseInfo = document.createElement("li");
-        courseInfo.classList.add('bg-gray-200', 'text-black', 'p-5', 'rounded-full', 'flex', 'flex-col');
+        courseInfo.classList.add(
+            "bg-gray-200",
+            "text-black",
+            "p-5",
+            "rounded-full",
+            "flex",
+            "flex-col",
+        );
         courseInfo.textContent = course.name;
 
         const detailsButton = document.createElement("button");
@@ -54,7 +54,6 @@ const loadCourses = async (
         courseInfo.appendChild(detailsButton);
         coursesList.appendChild(courseInfo);
     });
-
 };
 
 const showCourseDetails = (course: CourseResponseDto): void => {
@@ -62,15 +61,38 @@ const showCourseDetails = (course: CourseResponseDto): void => {
 
     const heading = document.createElement("h2");
     heading.textContent = course.name;
+    heading.classList.add("font-bold", "text-2xl");
 
     const description = document.createElement("p");
     description.textContent = course.description;
+
+    const spanStart = document.createElement("span");
+    const spanLength = document.createElement("span");
+    const spanLevel = document.createElement("span");
+    const spanPrice = document.createElement("span");
+
+    spanStart.textContent = `Start: ${course.startDate}`;
+    spanLength.textContent = `Längd: ${String(course.durationInWeeks)} veckor`;
+    spanLevel.textContent = `Nivå: ${CourseLevelLabels[course.level]}`;
+    spanPrice.textContent = `Pris: ${String(course.price)} kr`;
+
+    spanStart.classList.add("font-bold");
+    spanLength.classList.add("font-bold");
+    spanLevel.classList.add("font-bold");
+    spanPrice.classList.add("font-bold");
+
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("flex", "gap-5");
+
+    infoDiv.append(spanStart, spanLength, spanLevel, spanPrice);
 
     const applyButton = document.createElement("button");
 
     applyButton.type = "button";
     applyButton.textContent = "Anmäl dig nu";
-
+    applyButton.addEventListener("click", () => {
+        alert("Du är anmäld till kurs: " + course.name);
+    });
     applyButton.classList.add(
         "p-3",
         "bg-gray-950",
@@ -78,18 +100,13 @@ const showCourseDetails = (course: CourseResponseDto): void => {
         "rounded-lg",
         "hover:bg-gray-900",
         "hover:cursor-pointer",
-        "max-w-[20%]"
+        "max-w-[20%]",
     );
 
-    courseDetails.append(
-        heading,
-        description,
-        applyButton
-    );
+    courseDetails.append(heading, description, infoDiv, applyButton);
 };
 
 await loadCourses();
-
 
 courseSortSelect.addEventListener("change", async () => {
     const selectedValue = courseSortSelect.value;
@@ -117,7 +134,6 @@ courseSortSelect.addEventListener("change", async () => {
 
     await loadCourses(sortBy, direction);
 });
-
 
 function isCourseSortBy(value: string): value is CourseSortBy {
     return value === "price" || value === "level";
